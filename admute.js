87917b9db8001099f4a78fd20be76eaa6ca98cc1
Divyansh <divi09112@gmail.com>
2020-10-14 19:44:06 +0530

    don't unmute if user muted the player

--- admute.js
+++ admute.js
@@ -4,7 +4,8 @@ function sleep(ms) {
 
 const config = { attributes: true, childList: true, subtree: true };
 
-let muteState = 1; //1 = unmuted, 0 = muted
+let muteState = 1; // 1 = unmuted, 0 = muted
+let adMute = 0; // to keep track if the player was muted by the extension or the user
 let muteBtn;
 
 function getMuteBtn() {
@@ -44,9 +45,11 @@ getArtistDiv().then(()=>{
         if(artist === "Spotify") {
             if(muteState === 1) {
                 muteBtn.click();
+                adMute = 1;
             }
-        } else if(muteState === 0) {
+        } else if(adMute === 1 && muteState === 0) { // unmute only if muted by extension
             muteBtn.click();
+            adMute = 0;
         } else {
         }
     });
